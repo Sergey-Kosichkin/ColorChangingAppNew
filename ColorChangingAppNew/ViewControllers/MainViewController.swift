53e54ae9cc54567dda913a667199340a7452f466
Sergey Kosichkin <d978827@icloud.com>
2022-12-12 07:19:24 +0300

    Added text fields on settings screen Setup logic for color changing in settings

---
+++ ColorChangingAppNew/ViewControllers/MainViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MainViewController.swift
+//  ColorChangingAppNew
+//
+//  Created by Sergey Kosichkin on 11.12.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    private var viewColor = UIColor(red: CGFloat(0.55),
+                                    green: CGFloat(0.65),
+                                    blue: CGFloat(0.75),
+                                    alpha: CGFloat(1))
+    
+    override func viewWillAppear(_ animated: Bool) {
+        view.backgroundColor = viewColor
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {
+            return
+        }
+        settingsVC.viewColor = viewColor
+    }
+}
---
+++ ColorChangingAppNew/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,206 @@
+//
+//  SettingsViewController.swift
+//  ColorChangingAppNew
+//
+//  Created by Work on 02.12.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redTF: UITextField!
+    @IBOutlet var greenTF: UITextField!
+    @IBOutlet var blueTF: UITextField!
+    
+    
+    var viewColor = UIColor(red: 0,
+                            green: 0,
+                            blue: 0,
+                            alpha: 0)
+    
+    private var color: (red: CGFloat,
+                        green: CGFloat,
+                        blue: CGFloat,
+                        alpha: CGFloat)!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.hidesBackButton = true
+        mainView.layer.cornerRadius = 15
+        
+        setupColor()
+    }
+
+    @IBAction func redSliderAction() {
+        color.red = CGFloat(redSlider.value)
+        setupColorValues(withColor: .red,
+                         forLabel: true,
+                         slider: false,
+                         textField: true)
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+        color.green = CGFloat(greenSlider.value)
+        setupColorValues(withColor: .green,
+                         forLabel: true,
+                         slider: false,
+                         textField: true)
+    }
+    
+    @IBAction func blueSliderAction() {
+        color.blue = CGFloat(blueSlider.value)
+        setupColorValues(withColor: .blue,
+                         forLabel: true,
+                         slider: false,
+                         textField: true)
+    }
+    
+    @IBAction func redTFAction() {
+        color.red = color(fromString: redTF.text ?? "0")
+        setupColorValues(withColor: .red,
+                         forLabel: true,
+                         slider: true,
+                         textField: true)
+        
+    }
+    
+    @IBAction func greenTFAction() {
+        color.green = color(fromString: greenTF.text ?? "0")
+        setupColorValues(withColor: .green,
+                         forLabel: true,
+                         slider: true,
+                         textField: true)
+        
+    }
+    
+    @IBAction func blueTFAction() {
+        color.blue = color(fromString: blueTF.text ?? "0")
+        setupColorValues(withColor: .blue,
+                         forLabel: true,
+                         slider: true,
+                         textField: true)
+    }
+    
+    
+    @IBAction func doneButtonAction() {
+        
+    }
+    
+    
+}
+
+
+extension SettingsViewController {
+    
+    enum Coloring {
+        case red
+        case green
+        case blue
+    }
+    
+    
+    private func setupColorValues(withColor coloring: Coloring,
+                                  forLabel label: Bool,
+                                  slider: Bool,
+                                  textField: Bool) {
+        switch coloring {
+        case .red:
+            if label { redValueLabel.text = string(fromColor: color.red) }
+            if slider { animateSlider(with: color.red, andColoring: .red) }
+            if textField { redTF.text = string(fromColor: color.red) }
+        case .green:
+            if label { greenValueLabel.text = string(fromColor: color.green) }
+            if slider { animateSlider(with: color.green, andColoring: .green) }
+            if textField { greenTF.text = string(fromColor: color.green) }
+        case .blue:
+            if label { blueValueLabel.text = string(fromColor: color.blue) }
+            if slider { animateSlider(with: color.blue, andColoring: .blue) }
+            if textField { blueTF.text = string(fromColor: color.blue) }
+        }
+        changeColor()
+    }
+    
+    private func string(fromColor color: CGFloat) -> String {
+        String(format: "%.2f", color)
+    }
+    
+    
+    private func color(fromString string: String) -> CGFloat {
+        guard var float = Float(string) else { return 0 }
+        float = round(float * 100) / 100.0
+        
+        if float > 1 {
+            float = 1
+        } else if float < 0 {
+            float = 0
+        }
+        
+        return CGFloat(float)
+    }
+    
+    
+    private func animateSlider(with value: CGFloat, andColoring coloring: Coloring) {
+        let float = Float(value)
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            switch coloring {
+            case .red:
+                self.redSlider.setValue(float, animated:true)
+            case .green:
+                self.greenSlider.setValue(float, animated:true)
+            case .blue:
+                self.blueSlider.setValue(float, animated:true)
+            }
+        })
+    }
+    
+    
+    private func changeColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value),
+                                           green: CGFloat(greenSlider.value),
+                                           blue: CGFloat(blueSlider.value),
+                                           alpha: 1)
+    }
+    
+    
+    private func setupColor() {
+        color = viewColor.rgba
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        
+        setupColorValues(withColor: .red, forLabel: true, slider: true, textField: true)
+        setupColorValues(withColor: .green, forLabel: true, slider: true, textField: true)
+        setupColorValues(withColor: .blue, forLabel: true, slider: true, textField: true)
+        
+        mainView.backgroundColor = viewColor
+    }
+    
+}
+
+
+
+extension UIColor {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+        return (red, green, blue, alpha)
+    }
+}
